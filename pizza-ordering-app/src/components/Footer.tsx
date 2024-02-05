06787ef38f989a393d8ad072bf3acac07799a18f
PraveenBimsara <praveenbimsara99@gmail.com>
2024-02-05 23:07:07 +0530

    Finished home page and menu page

--- pizza-ordering-app/src/components/Footer.tsx
+++ pizza-ordering-app/src/components/Footer.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 function Footer() {
   return (
     <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-700 flex items-center justify-between'>
       <Link href={'/'} className='font-bold text-xl'>MASSIMO</Link>
       <p>ALL RIGHTS RESERVED.</p>
+      
     </div>
   )
 }
