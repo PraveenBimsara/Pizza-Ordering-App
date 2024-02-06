ee8601d7a6e27efe16d857dc0ddccaba0f3fc80f
PraveenBimsara <praveenbimsara99@gmail.com>
2024-02-06 23:50:21 +0530

    Created category page

--- pizza-ordering-app/src/app/product/[id]/page.tsx
+++ pizza-ordering-app/src/app/product/[id]/page.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Image from 'next/image'
+import { singleProduct } from '@/data'
+import Price from '@/components/Price'
 
 const SingleProductPage = () => {
   return (
-    <div>SingleProductPage</div>
+    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-700 md:flex-row'>
+      {/*IMAGE CONTAINER*/}
+      {singleProduct.img && (
+      <div className='relative'>
+        <Image src={singleProduct.img} alt='' fill className='object-contain'></Image>
+      </div>
+      )}
+      {/*TEXT CONTAINER*/}
+      <div className=''>
+        <h1>{singleProduct.title}</h1>
+        <p>{singleProduct.desc}</p>
+        <Price price={singleProduct.price} options={singleProduct.options} id={0}/>
+      </div>
+    </div>
   )
 }
 
---
+++ pizza-ordering-app/src/components/Price.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+type Props = {
+    price:number;
+    id:number;
+    options?: {title:string; additionalPrice: number}[]
+}
+
+const Price = ({price,id,options}: Props)=> {
+  return (
+    <div>
+        <h2>${price.toFixed(2)}</h2>
+        {/*OPTIONS CONTAINER*/}
+        <div className=''></div>
+        {/*QUANTITY AND ADD BUTTON CONTAINER*/}
+        <div className=''></div>
+    </div>
+  )
+}
+
+export default Price
